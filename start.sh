78271a877d3a83e2be306a410624b201339f7791
Hassan Ashraf Hassan Ali <enghassanashraf@gmail.com>
2026-03-09 16:52:53 +0200

    Update start.sh to use Django management commands
    Replace manual Django setup with management commands for migrations.

--- start.sh
+++ start.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -e
 cd /app/backend
-
-# This will print startup errors to Railway logs
-python -c "
-import os
-os.environ['DJANGO_ENV'] = 'production'  
-import django
-django.setup()
-print('Django OK')
-" || echo "DJANGO SETUP FAILED"
-
+python manage.py migrate 
+python manage.py migrate_schemas
+python manage.py migrate_schemas --shared
 gunicorn config.wsgi:application --config gunicorn.conf.py
