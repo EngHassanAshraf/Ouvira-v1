761f59d674929f9f7e3bcaff7ba083e56eb677ab
Hassan Ashraf Hassan Ali <enghassanashraf@gmail.com>
2026-03-09 11:47:35 +0200

    Modify start.sh for production environment setup
    Update start.sh to set DJANGO_ENV and check Django setup.

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
-cd backend
-python manage.py check --deploy  # this will print any startup errors
-python manage.py migrate_schemas --shared
-gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --log-level debug --capture-output
+cd /app/backend
+
+# This will print startup errors to Railway logs
+python -c "
+import os
+os.environ['DJANGO_ENV'] = 'production'  
+import django
+django.setup()
+print('Django OK')
+" || echo "DJANGO SETUP FAILED"
+
+gunicorn config.wsgi:application --config gunicorn.conf.py
