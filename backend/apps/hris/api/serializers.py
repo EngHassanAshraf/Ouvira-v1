80e90bce98e327dfd562b86b6351dc9747a2a612
enghassanashraf <enghassanashraf@gmail.com>
2026-03-11 17:05:46 +0200

    init hr module

---
+++ backend/apps/hris/api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from apps.hris.models import (
+    JobTitle, 
+    Location, 
+    Department, 
+    Position, 
+    Employee, 
+    Employment,
+    EmployeeAssignment,
+    EmployeeDocument,
+    EmployeeEvent,
+    AttendanceRecord,
+    LeaveRequest
+)
+
+# TODO: Add Serializers here mapping to models
